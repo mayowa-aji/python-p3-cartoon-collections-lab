c80b7fd09695fd879d8f291c16e75b43f04b932c
Mayowa Ajiboye <mayaajiboye@gmail.com>
2023-02-27 12:30:17 -0500

    Complete lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,36 @@
-def roll_call_dwarves():
-    pass
 
-def summon_captain_planet():
-    pass
+def roll_call_dwarves(dwarfs):
+    # for dwarf in dwarfs:
+    #     print (dwarfs[dwarf])
+    for i in range(len(dwarfs)):
+        print(f"{i+1}. {dwarfs[i]}")
 
-def long_planeteer_calls():
-    pass
+def summon_captain_planet(elements):
+    newlist = []
+    for element in elements:
+        newlist.append(element[0].upper() + element[1:]
+                       + "!")
+    return newlist
 
-def find_the_cheese():
-    pass
+def long_planeteer_calls(words):
+    for word in words:
+        if(len(word) > 4):
+            return True
+    return False
+
+# def long_planeteer_calls(words):
+#     words = [word for word in words if len(word) > 4 ]
+#     return len(words) > 0
+
+
+def find_the_cheese(list):
+    for i in range(len(list)):
+        if list[i]  == 'cheddar' or list[i] == 'gouda' or list[i] == 'camembert':
+            return list[i]
+    return None
+
+
+
+
+# test = summon_captain_planet(["feaea","feafe","fe", "fe", "fe"])
+# print(test)
